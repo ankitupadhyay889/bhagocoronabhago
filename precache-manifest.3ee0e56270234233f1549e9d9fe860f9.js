f7b0b4f3d048abc3d783a30993c2943b154a77a8
Ankit Upadhyay <56730118+ankitupadhyay889@users.noreply.github.com>
2020-07-27 14:46:14 +0530

    Updates

---
+++ precache-manifest.3ee0e56270234233f1549e9d9fe860f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aecc5d662bf4d6553872678eceb8458",
+    "url": "/bhagocoronabhago/index.html"
+  },
+  {
+    "revision": "ca943b196ba6f737fdc6",
+    "url": "/bhagocoronabhago/static/css/main.0722a84f.chunk.css"
+  },
+  {
+    "revision": "9ebb0e79c872cecaa542",
+    "url": "/bhagocoronabhago/static/js/2.5eb0aee9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bhagocoronabhago/static/js/2.5eb0aee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca943b196ba6f737fdc6",
+    "url": "/bhagocoronabhago/static/js/main.ae7cdf56.chunk.js"
+  },
+  {
+    "revision": "2410eee22ce84bc1ab8b",
+    "url": "/bhagocoronabhago/static/js/runtime-main.2b67b8d4.js"
+  }
+]);
